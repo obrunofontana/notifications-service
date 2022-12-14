0be516506cd2b5f55b7769c2840a467da8bc94b8
obrunofontana <obrunofontana@gmail.com>
2022-12-14 20:51:22 -0300

    feat: cria repositorio do prisma que implementa o create

---
+++ src/infra/database/prisma/repositories/PrismaNotificationRepository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Notification } from '../../../../app/entities/Notification';
+import { NotificationRepository } from '../../../../app/repositories/NotificationRepository';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+      },
+    });
+  }
+}
