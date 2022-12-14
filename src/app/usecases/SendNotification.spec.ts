9f1d985fae5097914053b3c6b19114d7b2fbc389
obrunofontana <obrunofontana@gmail.com>
2022-12-14 19:36:45 -0300

    test: garante que o repositorio e injetado no caso de uso

--- src/app/usecases/SendNotification.spec.ts
+++ src/app/usecases/SendNotification.spec.ts
@@ -1,16 +1,26 @@
 import { randomUUID } from 'crypto';
-import { SendNotification } from './SendNotitication';
+import { Notification } from '../entities/Notification';
+import { SendNotification } from './SendNotification';
+
+const notifications: Notification[] = [];
+
+// Mock
+const notificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       category: 'social',
       content: 'Nova solicitação de amizade',
       recipientId: randomUUID(),
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
