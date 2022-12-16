4da4070603e3bd3135b888dc9c82a6f138ffd1b1
obrunofontana <obrunofontana@gmail.com>
2022-12-16 16:50:47 -0300

    chore: adiciona index para exportar e facilitar as importações

---
+++ src/app/usecases/index.ts
@@ -0,0 +1,6 @@
+export * from './CancelNotification';
+export * from './CountRecipientNotifications';
+export * from './GetRecipientNotifications';
+export * from './ReadNotification';
+export * from './UnreadNotification';
+export * from './SendNotification';
