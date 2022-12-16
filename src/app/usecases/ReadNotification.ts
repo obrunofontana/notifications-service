fb8714b7dd312003330dd6ca616d1d335d7f0329
obrunofontana <obrunofontana@gmail.com>
2022-12-16 12:40:05 -0300

    feat: implementa read de notificação

---
+++ src/app/usecases/ReadNotification.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { NotificationRepository } from '../repositories/NotificationRepository';
+import { NotificationNotFound } from './errors/NotificationNotFound';
+
+interface ReadNotificationRequest {
+  notificationId: string;
+}
+
+type ReadNotificationResponse = void;
+
+@Injectable()
+export class ReadNotification {
+  constructor(private notificationRepository: NotificationRepository) {}
+
+  async execute(
+    request: ReadNotificationRequest,
+  ): Promise<ReadNotificationResponse> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) {
+      throw new NotificationNotFound();
+    }
+
+    notification.read();
+
+    this.notificationRepository.save(notification);
+  }
+}
